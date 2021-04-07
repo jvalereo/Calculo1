63bb52a6bd3a0b16b45b3b11384afdf45656d8a1
Jonas Valereo <jvalereo2016@gmail.com>
2021-04-07 15:30:06 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Calculo1
+ Programação Java básica - Calculo soma
---
+++ src/calculo1/Calculo1.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculo1;
+
+/**
+ *
+ * @author JONAS
+ */
+public class Calculo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Treinamento - Aula caculo soma, tipo int, concatenação
+        //Jonas valereo Técnico em Informática 
+        
+        //Declarando as variaveis tipo int
+        
+        int numero1 = 20;
+        int numero2 = 10;
+        
+        int resultado = numero1 + numero2;
+        
+        //Declarando o codigo imprimir na tela no console, concatenação
+        
+        System.out.println("O Resultado é: " + resultado);
+        
+         // Declarando saida da aplicação e descarrega a memoria
+        
+        System.exit(0);
+        
+        //Fim do programa
+        
+        
+       
+    }
+    
+}
